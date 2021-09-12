186d0d514c070b48b320e365bd2dd24f2b62a75d
Ian Santos <ian290303@gmail.com>
2021-09-12 15:50:52 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,5 +1,35 @@
+import axios from 'axios';
+import Pagiantion from 'components/pagination';
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+
 function DataTable() {
+    const[activePage, setActivePage] = useState(0);
+
+    const [page, setPage] = useState<SalePage> ({
+    last:true,
+    totalElements:0,
+    totalPages: 0,
+    number: 0,
+    first: true,
+    });
+
+    useEffect( () => {
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
+        .then(response => {
+            setPage(response.data);
+        })
+    }, [activePage])
+
+    const changePage = (index: number) => {
+        setActivePage(index); 
+    }
     return (
+        <>
+        <Pagiantion page={page} onPageChange = {changePage}/>
         <div className="table-responsive">
     <table className="table table-striped table-sm">
         <thead>
@@ -12,60 +42,20 @@ function DataTable() {
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
+            {page.content?.map(item => (
+            <tr key={item.id}>
+                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
             </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Barry Allen</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+            
+            ))} 
         </tbody>
     </table>
 </div>
-  
-      
+        </>
     );
   }
 export default DataTable;
